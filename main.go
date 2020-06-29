74e55012d6bddedd7b899dac7bd36e842326b44d
Tizen Ye <yzs981130@126.com>
2020-06-29 12:16:00 +0800

    basic implementation

--- main.go
+++ main.go
@@ -1,7 +1,131 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"os/exec"
+	"sync"
+	"text/template"
+	"time"
+)
+
+//{
+//    "data": [
+//        {
+//            "starttime": 3,
+//            "GPU": 8,
+//            "lasttime": 5
+//        },
+//        {
+//            "starttime": 3,
+//            "GPU": 8,
+//            "lasttime": 5
+//        }
+//    ]
+//}
+
+const (
+	imageName = "registry.sensetime.com/cloudnative122ai/nvidia/cuda-vector-add"
+)
+
+type TraceEntry struct {
+	Data []Data `json:"data"`
+}
+type Data struct {
+	Index       int
+	ImageName	string
+	StartTime   int `json:"startTime"`
+	GpuCnt      int `json:"gpuCnt"`
+	RunningTime int `json:"runningTime"`
+}
+
+var jobYamlTmpl = `apiVersion: v1
+kind: Pod
+metadata:
+  name: job-dispatcher-test-{{.Index}}
+spec:
+  restartPolicy: OnFailure
+  containers:
+  - name: cuda-vector-add
+    image: {{.ImageName}}
+    imagePullPolicy: Always
+    resources:
+      limits:
+        nvidia.com/gpu: {{.GpuCnt}}
+    volumeMounts:
+      - mountPath: /dev/shm
+        subPath: shm
+        name: dev
+    command: ["sleep"]
+    args: ["{{.RunningTime}}s"]
+`
+
+func dispatchJob(entry Data) error {
+	cmd := exec.Command("kubectl", "create", "-f", "-")
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+
+	tmpl, err := template.New("jobYaml").Parse(jobYamlTmpl)
+	if err != nil {
+		return err
+	}
+	err = tmpl.Execute(stdin, entry)
+	if err != nil {
+		return err
+	}
+
+	if out, err := cmd.CombinedOutput(); err != nil {
+		return err
+	} else {
+		log.Println(out)
+	}
+	return nil
+}
+
+func singleDispatcher(wg *sync.WaitGroup, entry Data) {
+	defer wg.Done()
+	time.Sleep(time.Duration(entry.StartTime) * time.Second)
+	log.Printf("dispatch job %d at %d\n", entry.Index, entry.StartTime)
+	err := dispatchJob(entry)
+	if err != nil {
+		log.Printf("dispatch job %d failed: %s\n", entry.Index, err)
+	}
+}
 
 func main() {
-	fmt.Println("start")
+	log.Println("start")
+	var wg sync.WaitGroup
+
+	inputString := `{
+	  "data": [
+		{
+		  "startTime": 3,
+		  "gpuCnt": 8,
+		  "runningTime": 5
+		},
+		{
+		  "startTime": 8,
+		  "gpuCnt": 2,
+		  "runningTime": 7
+		}
+	  ]
+	}`
+	inputData := []byte(inputString)
+	var traceEntries TraceEntry
+	err := json.Unmarshal(inputData, &traceEntries)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i, v := range traceEntries.Data {
+		log.Printf("load job %d: startTime %d, gpuCnt %d, runningTime %d\n",
+			i, v.StartTime, v.GpuCnt, v.RunningTime)
+		v.Index = i
+		v.ImageName = imageName
+		wg.Add(1)
+		go singleDispatcher(&wg, v)
+	}
+	wg.Wait()
+	log.Println("end")
 }
